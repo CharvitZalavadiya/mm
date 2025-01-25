efe19bb577818ccf33d2982966aca039e8fff13d
Charvit Zalavadiya <charvitzalavadiya777@gmail.com>
2025-01-25 11:29:46 +0530

    Retrieving th mongodbUri from .env

--- src/server/config/db.js
+++ src/server/config/db.js
@@ -1,6 +1,10 @@
 import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const uri = 'mongodb+srv://cz:cz@cluster0.uvhgvjz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// const uri = 'mongodb+srv://cz:cz@cluster0.uvhgvjz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const uri = process.env.MONGODB_URI
 const client = new MongoClient(uri);
 
 let db;
