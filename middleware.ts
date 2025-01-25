261ec50ef55c492b05072f89a7cf2ccbe33529ae
Charvit Zalavadiya <charvitzalavadiya777@gmail.com>
2025-01-25 17:57:32 +0530

    Conflit resolving 1

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,3 @@
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-// const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
-
-// export default clerkMiddleware((auth, request) => {
-//   if(!isPublicRoute(request)) {
-//     auth().protect();
-//   }
-// });
-
-// export const config = {
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', "/api/(.*)"],
-// };
-
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/forum(.*)', '/notes(.*)']);
@@ -20,17 +6,13 @@ export default clerkMiddleware(async (auth, req) => {
   const { userId, redirectToSignIn } = await auth()
 
   if (!userId && isProtectedRoute(req)) {
-    // Add custom logic to run before redirecting
-
     return redirectToSignIn()
   }
 })
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 }
