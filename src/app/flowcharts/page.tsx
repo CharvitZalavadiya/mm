908dee558cbbd3ccdf87272376d78bcecf1a550c
Charvit Zalavadiya <charvitzalavadiya777@gmail.com>
2025-02-07 18:29:49 +0530

    Created base UI for flowcharts

--- src/app/flowcharts/page.tsx
+++ src/app/flowcharts/page.tsx
@@ -1,8 +1,61 @@
+"use client";
+
+import SideBar from "@/layout/sidebar/page";
+import TopBarFlowcharts from "@/layout/topbarFlowcharts/page";
+import { Suspense, useState } from "react";
+import "./responsive.css"
+
 export default function FlowChart() {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [selectedColor, setSelectedColor] = useState<string>("allColor");
+  const [isSidebarVisible, setIsSidebarVisible] = useState<boolean>(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleCreateNewFlowchart = async () => {
+    console.log("clicked on pluse ");
+  };
+
+  const handleColorChange = (color: string) => {
+    setSelectedColor(color);
+  };
+
+  const handleCloseSidebar = () => {
+    if (isSidebarVisible) {
+      setIsSidebarVisible(false);
+    }
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+  };
+
+  const handleToggleSidebar = () => {
+    setIsSidebarVisible(!isSidebarVisible);
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
-    <main className="text-4xl p-32 w-fit text-gray-500 tracking-wide text-center leading-relaxed">
-      Flow chart page is this
+    <main className="flex justify-end w-screen p-3">
+      <div>
+        <Suspense fallback="sidebar">
+          {/* {isSidebarVisible && sidebarComp} */}
+          {isSidebarVisible && (
+            <SideBar isOpen={isSidebarOpen} onClose={handleCloseSidebar} />
+          )}
+        </Suspense>
+      </div>
+      <div className="cssMainCompNotes bg-sidebarGradient rounded-lg tracking-wide leading-relaxed h-[95dvh] overflow-y-scroll p-5 ml-3 w-full">
+        <span className="top-0 sticky">
+          <TopBarFlowcharts
+            onSearch={handleSearch}
+            onCreateFlowchart={handleCreateNewFlowchart}
+            onColorChange={handleColorChange}
+            selectedColor={selectedColor}
+            onToggleSidebar={handleToggleSidebar}
+          />
+        </span>
+      </div>
     </main>
   );
-};
+}
---
+++ src/layout/topbarFlowcharts/page.tsx
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from "react";
+// import "./animations.css"
+
+interface TopBarFlowchartsProps {
+  onSearch: (query: string) => void;
+  onCreateFlowchart: () => void;
+  onColorChange: (color: string) => void;
+  selectedColor: string;
+  onToggleSidebar: () => void;
+}
+
+const TopBarFlowcharts: React.FC<TopBarFlowchartsProps> = ({
+  onSearch,
+  onCreateFlowchart,
+  onColorChange,
+  selectedColor,
+  onToggleSidebar,
+}) => {
+  const [searchText, setSearchText] = useState("");
+  const [isMenuVisible, setIsMenuVisible] = useState(true);
+  const [placeholderText, setPlaceholderText] = useState("Search by Title");
+  const [selectedColorBorderWidth, setSelectedColorBorderWidth] =
+    useState("-2");
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMenuVisible(window.innerWidth < 850);
+      setPlaceholderText(
+        window.innerWidth > 650 ? "Search by Title" : "Search"
+      );
+      setSelectedColorBorderWidth(window.innerWidth > 385 ? "-2" : "");
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); 
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const colorArray = [
+    "allColor",
+    "pink",
+    "green",
+    "yellow",
+    "purple",
+    "red",
+    "cyan",
+    "gray",
+  ];
+
+  const returnBg = (pickColor: string) => {
+   
+
+    const baseClass = `${pickColor === selectedColor ? `border${selectedColorBorderWidth}` : ""}`;
+    switch (pickColor) {
+      case "allColor":
+        return `${baseClass} bg-allColor`;
+      case "pink":
+        return `${baseClass} bg-pink-500/50 hover:bg-pink-500/70`;
+      case "green":
+        return `${baseClass} bg-green-500/50 hover:bg-green-500/70`;
+      case "yellow":
+        return `${baseClass} bg-yellow-500/50 hover:bg-yellow-500/70`;
+      case "purple":
+        return `${baseClass} bg-purple-500/50 hover:bg-purple-500/70`;
+      case "red":
+        return `${baseClass} bg-red-500/50 hover:bg-red-500/70`;
+      case "cyan":
+        return `${baseClass} bg-cyan-500/50 hover:bg-cyan-500/70`;
+      case "gray":
+        return `${baseClass} bg-gray-500/50 hover:bg-gray-500/70`;
+      default:
+        return "";
+    }
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+    onSearch(event.target.value);
+  };
+
+  const handleColorClick = (color: string) => {
+    onColorChange(color);
+  };
+
+  const handleMenuClick = () => {
+    onToggleSidebar();
+  };
+
+  return (
+    <div className="flex gap-3 mb-3 select-none justify-evenly">
+      {isMenuVisible && (
+        <span
+          className="cssMenu hidden material-symbols-outlined py-2 cursor-pointer"
+          onClick={handleMenuClick}
+        >
+          menu
+        </span>
+      )}
+      <div className="cssTopbarFull flex gap-3 w-full">
+        <div className="cssTopbarSearchAndNew flex gap-3 w-2/3">
+          <span className="w-full bg-navBlockBackground rounded-full py-1 px-1 flex">
+            <span className="cssTopbarSearchIcon material-symbols-outlined rounded-full p-1 px-2 select-none hover:cursor-pointer">
+              search
+            </span>
+            <input
+              type="text"
+              value={searchText}
+              onChange={handleSearchChange}
+              className="cssTopbarSearch w-full px-2 bg-transparent focus:outline-none"
+              placeholder={placeholderText}
+            />
+          </span>
+          <button
+            onClick={onCreateFlowchart}
+            className="cssTopbarNew bg-navBlockBackground rounded-full py-2 px-3 flex place-content-center"
+          >
+            <span className="cssTopbarNewIcon material-symbols-outlined rounded-full select-none hover:cursor-pointer">
+              add
+            </span>
+          </button>
+        </div>
+        <span className="cssTopbarColors bg-navBlockBackground rounded-full py-1 px-3 max-w-1/3 overflow-x-auto flex gap-2">
+          {colorArray.map((color) => (
+            <ul className="py-1" key={color}>
+              <li
+                className={`${returnBg(
+                  color
+                )} cssTopbarColor w-6 h-6 rounded-full hover:cursor-pointer`}
+                onClick={() => handleColorClick(color)}
+              ></li>
+            </ul>
+          ))}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default TopBarFlowcharts;
